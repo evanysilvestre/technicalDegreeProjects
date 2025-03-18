c7b1676adf81614a2a3a24f19c113fcc527497a6
evanysilvestre <evany.120@gmail.com>
2025-03-18 16:42:29 -0300

    add new files

--- 2. background/script.js
+++ 2. background/script.js
@@ -2,18 +2,18 @@ const body = document.querySelector('body')
 const hexCode = document.querySelector('#hexCode')
 
 function changeColor() {
-
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
         for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
+            color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
-      } 
-      
-    body.style.backgroundColor = getRandomColor()
-  
-  hexCode.innerHTML = `The hex code of the color is ${getRandomColor()}`
+    }
+
+    const newColor = getRandomColor(); 
+
+    body.style.backgroundColor = newColor; 
+    hexCode.innerHTML = `The hex code of the color is ${newColor}`; 
 }
 
